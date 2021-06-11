8b597657783aff59ac83e7e47f0ac0999d81ee74
Cooper Jackson <davisjaxon@gmail.com>
2021-06-11 10:02:44 -0700

    migration, index, server, package files setup

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,14 @@
 // build your `/api/projects` router here
+const express = require('express')
+const helpers = require('./model')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+
+})
+
+router.post('/', (req, res, next) => {
+
+})
+
+module.exports = router
