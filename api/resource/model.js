84d767baf2e3c7d0117e708f41154a750120f13a
Cooper Jackson <davisjaxon@gmail.com>
2021-06-11 12:14:16 -0700

    got seeding working

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,6 @@
 // build your `Resource` model here
+
+
+// {resource_name: 'computer', resource_description: 'my windows desktop with my projects on it'},
+//         {resource_name: 'cleaning supplies', resource_description: 'bleach, clorox, cleaning wipes, paper towels, etc.'},
+//         {resource_name: 'big brain', resource_description: 'this task is going to require one huge brain, and i know a guy for the job'}
